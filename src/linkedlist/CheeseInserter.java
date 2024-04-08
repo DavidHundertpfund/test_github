e7f7b6190d8c7fa220ac3706d232caba04625ca3
DavidHundertpfund <david.hundertpfund@hak-reutte.ac.at>
2024-04-08 14:28:58 +0100

    coms

--- src/linkedlist/CheeseInserter.java
+++ src/linkedlist/CheeseInserter.java
@@ -1,5 +1,9 @@
 package linkedlist;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 public class CheeseInserter {
@@ -9,11 +13,29 @@ public class CheeseInserter {
 	private static final Pattern vegetables = Pattern.compile(
 			"Zucchini|Paprikas?|Tomaten?|Zwiebeln?"
 			);
+	
+	private static void insertCheeseAroundVeggetable(List<String> ingredients) {
+		//For Schleife alt Iterator als Laufvariable
+		for(ListIterator<String> iterator = ingredients.listIterator();
+				iterator.hasNext();) {
+			//Auslesen der momentanen Zutat
+			String ingredient = iterator.next();
+			//Überprüfen, ob die Zutat zu unserem Vegtable Pattern passt
+			if(vegetables.matcher(ingredient).matches()) {
+				iterator.add("Käse");
+			}
+		}
+						
+		
+		
+	}
 			
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<String>ingredients = new ArrayList<>();
+		Collections.addAll(ingredients, "Gnocchi", "Zucchini", "Paprika", "Milch", "Zwiebeln", "Melanzani");
+		insertCheeseAroundVeggetable(ingredients);
+		System.out.println(ingredients);
 	}
 
 }
